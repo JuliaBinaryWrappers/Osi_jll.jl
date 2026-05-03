a8e9dfa97a6ed4063401d137d8fc6a242c57e294
jlbuild <juliabuildbot@gmail.com>
2026-05-03 23:47:50 +0000

    Osi_jll build 0.10800.1200+0

--- src/Osi_jll.jl
+++ src/Osi_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Osi")
-JLLWrappers.@generate_main_file("Osi", UUID("7da25872-d9ce-5375-a4d3-7a845f58efdd"))
+JLLWrappers.@generate_main_file("Osi", Base.UUID("7da25872-d9ce-5375-a4d3-7a845f58efdd"))
 end  # module Osi_jll
